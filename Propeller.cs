99469c74311ef6a01989bc9cc9574c2984da73f2
RaulMonPi <al428109@uji.es>
2022-09-28 18:34:33 +0200

    Update Propeller.cs

--- Propeller.cs
+++ Propeller.cs
@@ -2,20 +2,3 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// Aquí se define la clase "propeller" y sus animaciones
-public class Propeller : MonoBehaviour
-{
-    public float propSpeed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        propSpeed = 5000;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Rotate(Vector3.forward * propSpeed * Time.deltaTime);
-    }
-}
